9ea7d953fa52b28b60e8a65570b937a607d4baec
KURITAYUUDAI <k024g0030@g.neec.ac.jp>
2025-10-09 15:13:01 +0900

    フォルダ分け

--- project/apprication/MyWorldMath.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyWorldMath.h"
-
-void WorldTransformUpdate(KamataEngine::WorldTransform& worldTransform)
-{
-	// 行列を計算
-	worldTransform.matWorld_ = MakeAffineMatrixB(worldTransform.scale_, worldTransform.rotation_, worldTransform.translation_);
-	// 定数バッファに転送
-	worldTransform.TransferMatrix();
-}
--- project/apprication/MyWorldMath.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "KamataEngine.h"
-#include "myMath.h"
-
-/// <summary>
-/// 行列を計算・転送する
-/// </summary>
-void WorldTransformUpdate(KamataEngine::WorldTransform& worldTransform);
